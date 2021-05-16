a9f141ba72a99772a21608eb8d11d83809802903
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-05-16 13:28:43 +0900

    テストデータを作成する。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Music.create(
+  name: 'MIX MACHINE〈 2021年 05月 13日 木 〉',
+  s3_key: 'MIXMACHINE/MIX MACHINE〈 2021年 05月 13日 木 〉.flac',
+  release_date: '2021-05-13'
+)
+
+Music.create(
+  name: 'MIX MACHINE〈 2021年 05月 12日 水 〉',
+  s3_key: 'MIXMACHINE/MIX MACHINE〈 2021年 05月 12日 水 〉.flac',
+  release_date: '2021-05-12'
+)
+
+Music.create(
+  name: 'MIX MACHINE〈 2021年 05月 11日 火 〉',
+  s3_key: 'MIXMACHINE/MIX MACHINE〈 2021年 05月 11日 火 〉.flac',
+  release_date: '2021-05-11'
+)
+
+Music.create(
+  name: 'MIX MACHINE〈 2021年 05月 10日 月 〉',
+  s3_key: 'MIXMACHINE/MIX MACHINE〈 2021年 05月 10日 月 〉.flac',
+  release_date: '2021-05-10'
+)
