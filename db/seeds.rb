8de44d8b09a8d5fabe270b6b8248452858baf2e2
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-05-19 10:19:30 +0900

    4月19日週のテストデータを挿入する。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Music.create(
+  name: 'MIX MACHINE〈 2021年 04月 19日 月 〉',
+  s3_key: 'MIXMACHINE/MIX MACHINE〈 2021年 04月 19日 月 〉.flac',
+  release_date: '2021-04-19'
+)
+
+Music.create(
+  name: 'MIX MACHINE〈 2021年 04月 20日 火 〉',
+  s3_key: 'MIXMACHINE/MIX MACHINE〈 2021年 04月 20日 火 〉.flac',
+  release_date: '2021-04-20'
+)
+
+Music.create(
+  name: 'MIX MACHINE〈 2021年 04月 21日 水 〉',
+  s3_key: 'MIXMACHINE/MIX MACHINE〈 2021年 04月 21日 水 〉.flac',
+  release_date: '2021-04-21'
+)
+
+Music.create(
+  name: 'MIX MACHINE〈 2021年 04月 22日 木 〉',
+  s3_key: 'MIXMACHINE/MIX MACHINE〈 2021年 04月 22日 木 〉.flac',
+  release_date: '2021-04-22'
+)
 
 Music.create(
   name: 'MIX MACHINE〈 2021年 05月 06日 木 〉',
