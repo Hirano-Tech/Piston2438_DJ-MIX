15b131a8aa98a03ec52813deaaf1fd4258117355
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-05-21 09:08:49 +0900

    シャッフル再生のためのコントローラを編集する。

--- app/controllers/music/all_shuffle_plays_controller.rb
+++ app/controllers/music/all_shuffle_plays_controller.rb
@@ -2,10 +2,16 @@ class Music::AllShufflePlaysController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    redirect_to(action: 'show', id: Music.readonly.sample)
+    if request.fullpath.include? 'all_mp3-shuffle'
+      redirect_to(action: 'show', id: MusicLibrary.readonly.where(category: 'MIX MACHINE（MP3）').select(:id).sample)
+    elsif request.fullpath.include? 'all_flac-shuffle'
+      redirect_to(action: 'show', id: MusicLibrary.readonly.where(category: 'MIX MACHINE（FLAC）').select(:id).sample)
+    end
+
+
   end
 
   def show
-    @music = Music.find(params[:id])
+    @music = MusicLibrary.readonly.select(:id, :name, :category, :s3_key, :release_date).find(params[:id])
   end
 end
--- app/controllers/music_players_controller.rb
+++ app/controllers/music_players_controller.rb
@@ -2,5 +2,7 @@ class MusicPlayersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @dj_all_mp3 = MusicLibrary.readonly.where(category: 'MIX MACHINE（MP3）').select(:id).sample
+    @dj_all_flac = MusicLibrary.readonly.where(category: 'MIX MACHINE（FLAC）').select(:id).sample
   end
 end
