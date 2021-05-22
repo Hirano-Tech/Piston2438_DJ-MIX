f0688581ef0c080c90090664128f120792f3b136
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-05-23 01:28:48 +0900

    直前に再生された 3曲が再生されないようにする。

--- app/controllers/music/all_shuffle_plays_controller.rb
+++ app/controllers/music/all_shuffle_plays_controller.rb
@@ -3,15 +3,33 @@ class Music::AllShufflePlaysController < ApplicationController
 
   def index
     if request.fullpath.include? 'all_mp3-shuffle'
-      redirect_to(action: 'show', id: MusicLibrary.readonly.where(category: 'MIX MACHINE（MP3）').select(:id).sample)
+      @music = MixmachineMp3Djmix.readonly.sample
+      while session[:already_play].include?(@music.id) do
+        @music = MixmachineMp3Djmix.readonly.sample
+      end
+
+      session[:already_play].unshift @music.id
+      session[:already_play].delete_at(3)
+      redirect_to(action: 'show', id: @music.id, category: @music.category.name)
     elsif request.fullpath.include? 'all_flac-shuffle'
-      redirect_to(action: 'show', id: MusicLibrary.readonly.where(category: 'MIX MACHINE（FLAC）').select(:id).sample)
+      @music = MixmachineFlacDjmix.readonly.sample
+      while session[:already_play].include?(@music.id) do
+        @music = MixmachineFlacDjmix.readonly.sample
+      end
+
+      session[:already_play].unshift @music.id
+      session[:already_play].delete_at(3)
+      redirect_to(action: 'show', id: @music.id, category: @music.category.name)
     end
 
 
   end
 
   def show
-    @music = MusicLibrary.readonly.select(:id, :name, :category, :s3_key, :release_date).find(params[:id])
+    if params[:category] == 'MIX MACHINE（MP3）'
+      @music = MixmachineMp3Djmix.readonly.select(:id, :name, :title, :s3_key, :release_date).find(params[:id])
+    elsif params[:category] == 'MIX MACHINE（FLAC）'
+      @music = MixmachineFlacDjmix.readonly.select(:id, :name, :title, :s3_key, :release_date).find(params[:id])
+    end
   end
 end
--- app/controllers/music_players_controller.rb
+++ app/controllers/music_players_controller.rb
@@ -2,7 +2,27 @@ class MusicPlayersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @dj_all_mp3 = MusicLibrary.readonly.where(category: 'MIX MACHINE（MP3）').select(:id).sample
-    @dj_all_flac = MusicLibrary.readonly.where(category: 'MIX MACHINE（FLAC）').select(:id).sample
+  end
+
+  def create
+    if params[:category] == nil
+      render action: :index
+    elsif params[:category] == 'MIX MACHINE（MP3）'
+      @music = MixmachineMp3Djmix.readonly.sample
+
+      session[:already_play] = Array.new(3)
+      session[:already_play].unshift @music.id
+      session[:already_play].delete_at(3)
+
+      redirect_to music_all_shuffle_play_path(id: @music.id, category: @music.category.name)
+    elsif params[:category] == 'MIX MACHINE（FLAC）'
+      @music = MixmachineFlacDjmix.readonly.sample
+
+      session[:already_play] = Array.new(3)
+      session[:already_play].unshift @music.id
+      session[:already_play].delete_at(3)
+
+      redirect_to music_all_shuffle_play_path(id: @music.id, category: @music.category.name)
+    end
   end
 end
