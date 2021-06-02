db0a15d2add38dd1c2592a59a5d8471e89fa4a6c
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-06-02 13:51:29 +0900

    過去 2週間分を再生するためのルーティングを編集する。

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 
   resources :music_players, only: [:index, :create]
   namespace :music do
-    resources :all_shuffle_plays, only: [:show]
-
-    get('all_mp3-shuffle', to: 'all_shuffle_plays#index')
-    get('all_flac-shuffle', to: 'all_shuffle_plays#index')
+    resources :all_shuffle_plays, only: [:index, :show]
+      get('all_mp3-shuffle', to: 'all_shuffle_plays#index')
+      get('all_flac-shuffle', to: 'all_shuffle_plays#index')
+    resources :latest2weeks, only: [:index, :show]
+      get('2weeks_mp3-shuffle', to: 'latest2weeks#index')
+      get('2weeks_flac-shuffle', to: 'latest2weeks#index')
   end
 end
