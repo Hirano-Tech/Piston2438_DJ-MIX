663f8ebdeb2a51c7747f9e11570c72431cb687b8
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-06-02 13:50:42 +0900

    過去 2週間分を再生する機能のためのフォーム内容を編集する。

--- app/controllers/music_players_controller.rb
+++ app/controllers/music_players_controller.rb
@@ -5,16 +5,19 @@ class MusicPlayersController < ApplicationController
   end
 
   def create
-    if params[:category] == nil
+    if params.permit(:play_method)[:play_method].blank?
       render action: :index
-    else
+    elsif params.permit(:play_method)[:play_method] == '01'
       @music = MixmachineDjmix.readonly.sample
-
-      session[:already_play] = Array.new(5)
-      session[:already_play].unshift @music.id
-      session[:already_play].delete_at(5)
-
-      redirect_to music_all_shuffle_play_path(id: @music.id, category: params[:category])
+        session[:already_play] = Array.new(5)
+        session[:already_play].unshift @music.id
+        session[:already_play].delete_at(5)
+      redirect_to music_all_shuffle_play_path(id: @music.id, category: params.permit(:category)[:category])
+    elsif params.permit(:play_method)[:play_method] == '02'
+      @musics = MixmachineDjmix.readonly.select(:id).order(release_date: :DESC).limit(8).shuffle
+      session[:playlists] = Array.new
+      session[:playlists] = @musics
+      redirect_to music_latest2week_path(id: @musics[0][:id], category: params.permit(:category)[:category])
     end
   end
 end
