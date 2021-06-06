717cb037f3be8cd639a65c9428cef6f9e65b9506
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-06-06 20:40:47 +0900

    特集別プレイリストのためのルーティングを記述する。

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
     resources :latest2weeks, only: [:index, :show]
       get('2weeks_mp3-shuffle', to: 'latest2weeks#index')
       get('2weeks_flac-shuffle', to: 'latest2weeks#index')
+    resources :playlist_themes, only: [:index, :show]
+      get('mp3-shuffle', to: 'playlist_themes#index')
+      get('flac-shuffle', to: 'playlist_themes#index')
   end
 end
